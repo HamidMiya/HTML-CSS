4159aeffbcd47b17645cac7e8d2e69b134309a00
Hamid Miya <hamidmiya440@gmail.com>
2024-08-31 01:55:40 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,218 @@
+// a=120;
+// b= 310;
+// console.log(a>b || b>a);
+
+
+// let name5 ="Miyaa";
+// if(name5 == "Miya"){
+//     console.log("Wellcome");
+// }
+// else if(name5!="Miya"){
+//     console.log("Use your own Identity");
+// }
+// else
+// {
+//     console.log("Try again");
+// }
+
+
+// num=11;
+// if(num%2 ==0){
+//     console.log(num," is Even");}
+// else{
+//     console.log(num," is Odd")}
+
+
+// let age = 122;
+// if(age <= 12){     console.log("You are kid take a glass of Milk & go your house soon");               }
+// else if(age >= 12 && age <= 18)  {         console.log("you are teen ager stear clear of bad things")                   }
+// else{    console.log("Do what you want");           }
+
+
+// let use=prompt("What is your name");
+// console.log(use);
+
+
+// for(let i=1; i<=10; i++){
+//     console.log("Hello Every One ");        }
+// For Making 2 Table 
+// for(i=1; i<=10; i++){
+//     console.log("2 * ",i,"=",i*2);             }
+
+
+// for(let i=1; i<=10; i++){
+//     console.log("i is = ", i);}
+
+
+// j=1;
+// while(j<=10){
+//     j++;
+//     console.log(j);}
+
+
+// let str="HAMID MIYA";
+// for(let i of str){
+//     console.log(i);   }
+
+
+
+
+// let str="Hamid";
+// for (i of str){
+//     console.log(i);
+
+// }
+
+// let student={
+//     name:"HAMID MIYA",
+//     age :21,
+//     qualification : "BSIT (Hons)",
+//     specialist : "Classified Subjuect Speclist "
+
+// };
+
+// for(let key in student){
+//     console.log(key);
+
+// }
+
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+
+//     }
+
+// }
+
+
+// for(let i =1; i<=100; i++){
+//     if(i%2 !==0){
+//         console.log(i);
+
+//     }
+// }
+
+
+// let obj={
+//     name:"HAMID MIYA",
+//     age : 21,
+//     Area : "Hyderabad"
+// };
+// let literal =`This is Special Literal by ${obj.Area} ${obj.name}`;
+// console.log(literal);
+
+
+// let str = "hamid miya";
+// let str2 = str.toUpperCase(str);
+// console.log(str.replace("hamid" , "Bhai"));
+// console.log(str.charAt(2));
+
+
+// Apna College last Project of String Lec 2 Loops Video
+// console.log(str);
+// console.log(str2);
+// let username = prompt("Enter your Name");
+// console.log("Your name is ", username);
+// let ans = "@" + username + username.length;
+// console.log(ans);
+
+
+// let name = ["Muhammad", "Hamid", "Miya", "Shaikh"];// Array 
+// console.log(name[1]);
+
+
+// function school() {    console.log("This is a Government School");
+//     console.log("This is Brillant school");}school();
+
+
+// function sum(C,Python,Java, JavaScript,php ) {
+//     result = C+Python+Java+ JavaScript+php;
+//     console.log('This is your Coding Marks');
+//         return result;
+//     return php;
+// }
+// let final = sum(10,20,30,40,50);
+// console.log(final);
+
+
+// function sum(a,b) {
+//     // result= a + b;
+//     // return result;
+//     console.log(a+b);   };
+// // let final= sum(3,3);
+// // console.log(final);
+// sum(4,6);
+
+// function minus(a,b) {
+//     console.log(a-b); }
+// minus(220,10)
+
+// function divide(a,b) {
+//     console.log(a/b);}
+// divide(100,50)
+
+// function multiply(a,b) {
+//     console.log(a*b);}
+// multiply(100,50)
+
+// const fun1 = (a, b) => {
+//     console.log(a * b);
+// }
+// fun1(20, 10)
+
+// const fun2 = (a,b) => {
+//     return a*b;
+// }
+// fun2(5,50);
+
+
+// AI code 
+// const fun2 = (a, b) => {
+//     return a * b;
+// }
+
+// const result = fun2(5, 50);
+// console.log(result); // This will print 250 to the console
+
+// let fun3 = () =>{
+//     console.log("This is our Aerrow Function ");}
+// fun3();
+
+
+// Practiceing Function
+// function countVowels(str) {
+//     let count = 0;
+//     for (const char of str) {
+//         if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+//             count++;
+//         }
+//     }
+//     return count;
+// }console.log(countVowels("Hamid Miya")); // This will output the number of vowels in the string "example"
+
+
+// let array = ["Muhammad" , "Hamid" , "Miya"];
+// array.forEach(function fun1(val){
+// console.log(val);
+// }); /// can be done with for Loop present on line 53
+
+// let a =[1,2,3,4,5];
+// for(let i of a){
+//     console.log(i);   }
+
+
+    // let str="HAMID MIYA";
+    // for(let i of str){
+    //     console.log(i);   }
+    window.console.log("Hy");
+
+
+
+
+
+
+
+
+
+
